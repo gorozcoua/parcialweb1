70cc2b4d098a0cd77bd9b211e0789cee31bae9af
gorozcoua <g.orozcos@uniandes.edu.co>
2024-11-03 18:21:46 -0500

    Punto 2: Modifique el componente de listar para incluir dos párrafos que muestren el número de variedades de café por tipo proporcionadas por el listado

--- parcial/src/app/cafes/cafes-list/cafes-list.component.ts
+++ parcial/src/app/cafes/cafes-list/cafes-list.component.ts
@@ -10,13 +10,22 @@ import { CafeService } from '../cafe.service';
 export class CafesListComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
+  blend: number = 0;
+  origen: number = 0;
+
 
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void {
     this.cafeService.getCafes().subscribe((cafes) => {
       this.cafes = cafes;
+      this.calculateTypes();
     });
+  }
+
+  calculateTypes(): void {
+    this.origen = this.cafes.filter(cafe => cafe.tipo.includes("Origen")).length;
+    this.blend = this.cafes.filter(cafe => cafe.tipo.includes("Blend")).length;
   }
  
   ngOnInit() {
