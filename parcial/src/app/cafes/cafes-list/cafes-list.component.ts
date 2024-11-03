4758a7cffa1c2dd05a51aa36a5b4426680563863
gorozcoua <g.orozcos@uniandes.edu.co>
2024-11-03 18:05:06 -0500

    Un componente de listar café q ue presentará en una tabla de Bootstrap los datos del café ( nombre, tipo y región )

---
+++ parcial/src/app/cafes/cafes-list/cafes-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Cafe } from '../cafe';
+import { CafeService } from '../cafe.service';
+
+@Component({
+  selector: 'app-cafes-list',
+  templateUrl: './cafes-list.component.html',
+  styleUrls: ['./cafes-list.component.css']
+})
+export class CafesListComponent implements OnInit {
+
+  cafes: Array<Cafe> = [];
+
+  constructor(private cafeService: CafeService) { }
+
+  getCafes(): void {
+    this.cafeService.getCafes().subscribe((cafes) => {
+      this.cafes = cafes;
+    });
+  }
+ 
+  ngOnInit() {
+    this.getCafes();
+  }
+
+}
--- parcial/src/app/cafes/cafes.module.ts
+++ parcial/src/app/cafes/cafes.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CafesComponent } from './cafes.component';
+import { CafesListComponent } from './cafes-list/cafes-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CafesComponent],
-  exports: [CafesComponent]
+  declarations: [CafesListComponent],
+  exports: [CafesListComponent]
 })
 export class CafesModule { }
